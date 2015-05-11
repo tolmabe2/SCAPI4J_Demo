e8fea844020cdbcb3a0d6179bd764d9e36f9983a
Behar Mazreku <behar.mazreku@swisscom.com>
2015-05-11 14:57:46 +0200

    Die Test Klasse wurde angepasst

--- SCAPI4J_Demo/src/test.java
+++ SCAPI4J_Demo/src/test.java
@@ -1,9 +1,10 @@
+import helloWorld.helloWorld;
 
 public class test {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("Behar");
+		helloWorld.sayInLog4j("Test");
+		
 	}
 
 }
